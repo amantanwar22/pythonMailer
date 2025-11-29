16078243618be631299b9f50c6c6c162d3dad009
Aman Tanwar <amantanwar1055@gmail.com>
2025-11-29 17:25:35 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+### Steps
+
+## Creating google app password
+
+Step 1: Go to your google account -> then security -> then enable 2-step verification if its off -> also disable any passkeys/google prompt
+
+Step 2: Now login using your google account on this link: https://myaccount.google.com/apppasswords -> then it will ask for app name, so write app name -> then it will show you a 16-character password, copy it.
+
+---
+
+## Code walkthrough
+
+1: Only dependency to install is pandas so Install pandas:
+
+pip install pandas
+
+
+2: In line 6,7 of main.py edit the sender email and in app password the 16 digit code we copied earlier.
+
+3: You can change Subject at line 18 in main.py
+
+4: Now change the csv file accordingly and then you can run the file.
+
+It will display the names in the console of whether someone has received it or if there is an error.
+
+---
+
+### Output sample
+![Output Sample](./sample.png)
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,34 @@
+import smtplib
+import pandas as pd
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+SENDER_EMAIL = "jsforwork531@gmail.com"
+APP_PASSWORD = "yajdpbqeznxulizc"
+# RECEIVER_EMAIL = "aparneshgaurav@gmail.com"
+df = pd.read_csv("emails.csv")
+
+SUBJECT = "Test mail using Python"
+BODY = "Hello, this mail has been sent using Python and Gmail SMTP"
+
+def send_email(to_email,message_body):
+    msg = MIMEMultipart()
+    msg["From"] = SENDER_EMAIL
+    msg["To"] = to_email
+    msg["Subject"] = "Testing"
+    msg.attach(MIMEText(message_body, "plain"))
+
+    try:
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login(SENDER_EMAIL, APP_PASSWORD)
+
+        server.sendmail(SENDER_EMAIL, to_email, msg.as_string())
+        server.quit()
+
+        print(f"Email sent to {to_email}")
+    except Exception as e:
+        print(f"Error sending email to {to_email}:", e)
+
+for index, row in df.iterrows():
+    send_email(row['email'], row['message'])
